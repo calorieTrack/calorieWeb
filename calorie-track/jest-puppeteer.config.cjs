70b4553de7cc8f40636184a855c43242c5038b25
Isaac Lee <isaaclee2006@ucla.edu>
2025-12-05 21:49:52 -0800

    Resolving merge conflict

--- calorie-track/jest-puppeteer.config.cjs
+++ calorie-track/jest-puppeteer.config.cjs
@@ -1,3 +1,5 @@
+const isCI = process.env.CI === 'true';
+
 // Prompt: I keep getting "about:blank" when attempting to load the project onto the puppeteer test environment.
 // However, when I run the project normally with "npm run dev", it works fine. How do fix this issue?
 // Result:
@@ -5,6 +7,12 @@ module.exports = {
   launch: {
     headless: true,
     slowMo: 50,
+    args: isCI ? [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ] : [],
   },
   
   server: {
